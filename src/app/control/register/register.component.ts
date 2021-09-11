0cd9beb4f6539ddb076838ed5713f7f4a30e2fdc
archana3001 <56291695+archana3001@users.noreply.github.com>
2021-09-11 22:21:52 +0530

    Update register.component.ts

--- src/app/control/register/register.component.ts
+++ src/app/control/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from "@angular/forms";
+import { FormBuilder, FormGroup , Validators} from "@angular/forms";
 import { AuthService } from './../../shared/auth.service';
 import { Router } from '@angular/router';
 
@@ -17,10 +17,10 @@ export class RegisterComponent implements OnInit {
     public router: Router
   ) { 
   this.signupForm = this.fb.group({
-    name: [''],
-    email: [''],
-    password: [''],
-    admintype:['']
+    name: ['', Validators.required, Validators.minLength(3)],
+    email: ['', Validators.required, Validators.email],
+    password: ['', Validators.required, Validators.minLength(6)],
+    admintype:['', Validators.required]
   })
 }
   ngOnInit(): void {}
