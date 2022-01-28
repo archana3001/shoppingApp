81cced33bb5c2cffa8a35d1b5848edbfb98c2974
archana3001 <56291695+archana3001@users.noreply.github.com>
2022-01-29 02:09:46 +0530

    Update db.js

--- server/database/db.js
+++ server/database/db.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 module.exports = {
-    db: 'mongodb+srv://<username>:<password>@cluster0.h3dfx.mongodb.net/<collection>?retryWrites=true&w=majority'
+    db: process.env.DATABASE;
 }
